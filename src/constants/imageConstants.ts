feaae88b757bab4e09a0398474c75e9a7d8bac74
hwang <>
2022-06-29 14:55:42 +0900

    Add: 이미지 상수 처리

---
+++ src/constants/imageConstants.ts
@@ -0,0 +1,7 @@
+import tripleLogo from '../images/triple2x.png'
+import appStore from '../images/badge-apple4x.png'
+import playStore from '../images/play-store2x.png'
+
+export const TRIPLE_LOGO = tripleLogo
+export const APP_STORE = appStore
+export const PLAY_STORE = playStore
