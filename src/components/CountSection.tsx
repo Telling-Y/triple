1d811cc57b45447f055ff7aa940cb326db4f50ea
hwang <>
2022-06-29 15:53:27 +0900

    Add: CountSection 내 컴포넌트 호출 수정사항: - ContentLogo, Metrics, Awards

--- src/components/CountSection.tsx
+++ src/components/CountSection.tsx
@@ -1,8 +1,18 @@
 import styled from 'styled-components'
 
+import { Awards } from './Awards'
+import { ContentLogo } from './ContentLogo'
+import { Metrics } from './Metrics'
+
 const CountSectionContainer = styled.section``
 
 const CountSection: React.FC = () => {
-  return <CountSectionContainer>Section</CountSectionContainer>
+  return (
+    <CountSectionContainer>
+      <ContentLogo />
+      <Metrics />
+      <Awards />
+    </CountSectionContainer>
+  )
 }
 export default CountSection
