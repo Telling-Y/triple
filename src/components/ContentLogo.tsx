f7efdc63b0762e6f52ab3a7ed40357ae96d5435e
hwang <>
2022-06-29 15:10:25 +0900

    Add: ContentLogo 컴포넌트 생성

---
+++ src/components/ContentLogo.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const ContentLogoBox = styled.div``
+
+export const ContentLogo: React.FC = () => {
+  return <ContentLogoBox>2021년 12월 기준</ContentLogoBox>
+}
