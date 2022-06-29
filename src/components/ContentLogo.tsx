0cf569ef146a91618b415394686bda1278f26496
hwang <>
2022-06-29 15:56:49 +0900

    Modify: ContentLogo 컴포넌트 style 변경 수정사항: - 로고 이미지 상수 호출 - 스타일 적용

--- src/components/ContentLogo.tsx
+++ src/components/ContentLogo.tsx
@@ -1,6 +1,19 @@
 import styled from 'styled-components'
 
-const ContentLogoBox = styled.div``
+import { TRIPLE_LOGO } from '../constants/imageConstants'
+
+const ContentLogoBox = styled.div`
+  width: 400px;
+  height: 338px;
+  padding-top: 280px;
+  background-image: url(${TRIPLE_LOGO});
+  background-size: 400px 338px;
+  background-repeat: no-repeat;
+  text-align: center;
+  font-family: sans-serif;
+  font-size: 15px;
+  color: rgba(58, 58, 58, 0.7);
+`
 
 export const ContentLogo: React.FC = () => {
   return <ContentLogoBox>2021년 12월 기준</ContentLogoBox>
