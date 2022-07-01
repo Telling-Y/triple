8822257fdea7cfc87e1442be797ac655727e9039
hwang <>
2022-07-01 15:06:28 +0900

    Modify: 변수명 변경 수정사항: 가독성, 의미 고려한 변수명 변경(frame->viewSpeed, proportion->frame)

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -5,20 +5,20 @@ import { easeOutExpo } from 'utils/easeOutExpo'
 export const useCounter = (maximum: number): number => {
   const [returnCount, setReturnCount] = useState(0)
   const duration = 2000
-  const frame = 1000 / 120
-  const proportion = Math.floor(duration / frame)
+  const viewSpeed = 1000 / 120
+  const frame = Math.floor(duration / viewSpeed)
   useEffect(() => {
     let count = 0
     const counter = setInterval(() => {
-      const progress = easeOutExpo(count++ / proportion)
+      const progress = easeOutExpo(count++ / frame)
       setReturnCount(Math.floor(maximum * progress))
-      if (count > proportion) {
+      if (count > frame) {
         clearInterval(counter)
       }
       return () => {
         clearInterval(counter)
       }
     }, frame)
-  }, [frame, maximum, proportion])
+  }, [maximum, viewSpeed, frame])
   return returnCount
 }
