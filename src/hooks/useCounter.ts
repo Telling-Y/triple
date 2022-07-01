8f40f3287e6877a7e23377aca1aeffcc9975bf33
hwang <>
2022-07-01 10:16:35 +0900

    Fix: useCounter의 동일 시간 종료 위한 로직 변경 수정사항: - for문에서 setInterval로 변경 - 증가비를 활용해 동일 시간에 끝나도록 처리

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,13 +1,19 @@
 import { useEffect, useState } from 'react'
 
-export const useCounter = (maximum: number) => {
+export const useCounter = (maximum: number): number => {
   const [returnCount, setReturnCount] = useState(0)
+  const duration = 2000
+  const frame = 1000 / 120
+  const proportion = Math.floor(duration / frame)
   useEffect(() => {
-    for (let count = 0; count <= maximum; count++) {
-      setTimeout(() => {
-        setReturnCount(count)
-      }, 1000)
-    }
-  }, [])
+    let count = 0
+    const counter = setInterval(() => {
+      const progress = count++ / proportion
+      setReturnCount(Math.floor(maximum * progress))
+      if (count > proportion) {
+        clearInterval(counter)
+      }
+    }, frame)
+  }, [frame, maximum, proportion])
   return returnCount
 }
